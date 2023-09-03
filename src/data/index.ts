5a4adfd9755bbd5450fe0093834be270547b8aca
John Barnes <ohenesetwumasi@gmail.com>
2023-09-03 01:37:44 +0000

    feat: initial

---
+++ src/data/index.ts
@@ -0,0 +1,45 @@
+export const pies = [
+  {
+    name: "Pumpkin",
+    description: "Delicious Pumpkin Pie",
+    image: "",
+    assets: [],
+  },
+
+  {
+    name: "Strawbery",
+    description: "Delicious Pumpkin Pie",
+    image: "",
+    assets: [],
+  },
+  {
+    name: "Caramel",
+    description: "Delicious Pumpkin Pie",
+    image: "",
+    assets: [],
+  },
+  {
+    name: "Lemon",
+    description: "Delicious Pumpkin Pie",
+    image: "",
+    assets: [],
+  },
+  {
+    name: "Currant",
+    description: "Delicious Pumpkin Pie",
+    image: "",
+    assets: [],
+  },
+  {
+    name: "Blackberry",
+    description: "Delicious Pumpkin Pie",
+    image: "",
+    assets: [],
+  },
+  {
+    name: "Apple",
+    description: "Delicious Apple Pie",
+    image: "",
+    assets: [],
+  },
+];
